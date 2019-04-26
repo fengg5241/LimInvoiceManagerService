e598e2259a7970fdfa3b5d2ec2a4ab8d32c36d81
feng <fengg_5241@163.com>
2019-04-26 18:15:58 +0800

    Add Company CRUD API

--- src/main/java/com/lim/cms/controller/CompanyController.java
+++ src/main/java/com/lim/cms/controller/CompanyController.java
@@ -3,6 +3,7 @@ package com.lim.cms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,11 @@ public class CompanyController {
 	@RequestMapping(value = "/selectAll", produces = "application/json")
 	public List<SimCompany> selectAll() {
 		return companyService.selectAll();
+	}
+	
+	@RequestMapping(value = "/selectById/{id}", produces = "application/json")
+	public SimCompany selectById(@PathVariable int id) {
+		return companyService.selectByPrimaryKey(id);
 	}
 	
 	@PostMapping("/insert")
